d0f562a3ec4f5d590d0d8d02d7af767a9b1db6e8
Parth <pskuri@gmail.com>
2022-01-31 21:06:58 -0500

    Create asyncio_print.py
    Replace await asyncio.sleep(1) with time.sleep(1) to see the difference in run time

---
+++ asyncio/asyncio_print.py
@@ -0,0 +1,18 @@
+import asyncio
+import time
+
+async def printasync(s):
+    print(s)
+    await asyncio.sleep(1)
+
+# loop to execute my tasks
+start = time.time()
+loop = asyncio.get_event_loop()
+tasks = [
+    loop.create_task(printasync("hi")),
+    loop.create_task(printasync("bye!")),
+]
+loop.run_until_complete(asyncio.wait(tasks))
+loop.close()
+end = time.time()
+print(f'Time: {end-start:.2f} sec')
